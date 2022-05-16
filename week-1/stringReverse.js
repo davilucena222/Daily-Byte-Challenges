7206a4be7d20805389fce683d258a631e88caec1
Davi Luiz <davilucena222@gmail.com>
2022-05-15 22:47:40 -0300

    exercise 1 - week 1

---
+++ week-1/stringReverse.js
@@ -0,0 +1,12 @@
+function reverseString(string) {
+  let keepString = string.split(""); //splitting string ["D", "a", "v", "i", "L", "u", "i", "z"]
+
+  let reverseArrayForString = keepString.reverse(); //inverting the string ["z", "i", "u", "L", "i", "v", "a", "D"]
+
+  let joinString = reverseArrayForString.join(""); //join all the characters [z, i, u, L, i, v, a, D]
+
+  return joinString; //returning [z, i, u, L, i, v, a, D]
+
+}
+
+console.log(reverseString("DaviLuiz"));
