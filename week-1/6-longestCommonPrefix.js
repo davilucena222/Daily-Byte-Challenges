0c823d31302db11737bb53fcd3481f07a413521a
Davi Luiz <davilucena222@gmail.com>
2022-05-20 22:47:38 -0300

    6th exercise updated

--- week-1/6-longestCommonPrefix.js
+++ week-1/6-longestCommonPrefix.js
@@ -1,5 +1,9 @@
 function longestCommonPrefix(stringsArray){
+  let stringOne = stringsArray[0];
+  let stringTwo = 0;
   let result = "";
+  let stopCondition = 0;
+
   if(stringsArray.length == 0){
     return "";
   }
@@ -8,23 +12,35 @@ function longestCommonPrefix(stringsArray){
     return stringsArray[0];
   }
 
-  for(let i = 0; i < stringsArray[0].length; i++){
-    for(let j = 1; j < stringsArray.length; j++){
-      if(stringsArray[j][i] == stringsArray[0][i]){
-        continue;
-      } else {
-        return result;
-      }
+  //getting the second index of the array to compare with the first index
+  for(let x = 1; x < stringsArray.length; x++) {
+    stringTwo = stringsArray[x];
+
+    //comparing both strings length to get the lowest and use as the stop condition
+    if(stringOne.length < stringTwo.length) {
+      stopCondition = stringOne;
+    } else {
+      stopCondition = stringTwo;
     }
 
-    result += stringsArray[0][i];
+    //assigning an empty string to the result variable to prevent concatenation with the previous string
+    result = "";
+    for(let k = 0; k < stopCondition.length; k++) {
+      //comparing each character between the first and second string
+      if(stringOne[k] === stringTwo[k]){
+        //storing the prefix value 
+        result += stopCondition[k];
+      } else {
+        //if the stop condition is broken then break
+        break;
+      }
+    }
+    //storing the prefix value in stringOne to comparing with the other string in the array
+    stringOne = result;
   }
   return result;
 }
-console.log(longestCommonPrefix(["colorado", "color", "cold", "coll"]));
+
+console.log(longestCommonPrefix(["colorado", "color", "cold"]));
 console.log(longestCommonPrefix(["a", "b", "c"]));
 console.log(longestCommonPrefix(["spot", "spotty", "spotted"]));
-
-// ["colorado", "color", "cold"], return "col"
-// ["a", "b", "c"], return ""
-// ["spot", "spotty", "spotted"], return "spot"
