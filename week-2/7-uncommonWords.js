958a93be6c564ea5ad4f37056c6103bf14511c4f
Davi Luiz <davilucena222@gmail.com>
2022-05-29 15:19:05 -0300

    time complexity update"

--- week-2/7-uncommonWords.js
+++ week-2/7-uncommonWords.js
@@ -1,17 +1,20 @@
+//time complexity = O(N^2)
 function uncommonWord(word1, word2){
   let splittingWord1 = word1.split(" ");
   let splittingWord2 = word2.split(" ");
   let result = [];
+  let string1 = " ";
+  let string2 = " ";
 
-  for(let string1 of splittingWord1){
-    if(!(splittingWord2.includes(string1))){
+  for(string1 of splittingWord1){
+    if(!splittingWord2.includes(string1)){
       result.push(string1);
     }
   }
 
-  for(let string2 of splittingWord2){
-    if(!(splittingWord1.includes(string2))){
-      result.push(string2)
+  for(string2 of splittingWord2){
+    if(!splittingWord1.includes(string2)){
+      result.push(string2);
     }
   }
 
