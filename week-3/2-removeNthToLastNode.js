be038429592f7fe3ddcd2b13deb9f7904b31a396
Davi Luiz <davilucena222@gmail.com>
2022-05-30 13:06:48 -0300

    2nd exercise week 3

---
+++ week-3/2-removeNthToLastNode.js
@@ -0,0 +1,49 @@
+/* 
+  This question is asked by Facebook. Given a linked list and a value n, remove the nth to last node and return the resulting list.
+
+  Ex: Given the following linked lists...
+
+  1->2->3->null, n = 1, return 1->2->null
+  1->2->3->null, n = 2, return 1->3->null
+  1->2->3->null, n = 3, return 2->3->null
+*/
+
+class ListNode {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+//time complexity = O(N)
+//space complexity = O(1)
+function removeNthToLastNode(list, n){
+  //creating the list node and initializing with -1 that is gonna point to the next, the next is the first element of the list
+  let dummyHead = new ListNode(-1);
+  dummyHead.next = list; //pointing the dummyHead for the first element of the list 
+  let currentResult = dummyHead; //storing the current result dummyHead for incrementing 
+  let counter = 0; //counter to calculate the index that will be removed
+  let tail = list;  //the tail of the list or poiting to the first element
+
+  while(counter < n){ //while counter is smaller than the index that will be removed
+    counter += 1;
+    tail = tail.next; //incrementing tail
+  }
+
+  let removeNode = list; //assigning removeNode to the list
+  let previous = dummyHead; //assing previous to the current result of the dummyHead
+
+  while(tail){ //while tail exists
+    tail = tail.next; //incrementing tail
+    removeNode = removeNode.next; //incrementing removeNode
+    previous = previous.next; //incrementing previous
+  }
+
+  previous.next = removeNode.next; //assigning previous to the next item (the item that is after the position of the item that was removed)
+
+  return currentResult.next;
+}
+
+//input examples
+removeNthToLastNode([1, 2, 3], 1);
+removeNthToLastNode([1, 2, 3], 2);
+removeNthToLastNode([1, 2, 3], 3);
